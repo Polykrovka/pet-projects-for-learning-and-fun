f6e95e5c571f45e9ad48b68ba82598a2c9b6c6ff
Polykrovka <2595092@gmail.com>
2021-07-25 01:59:55 +0300

    add checkers on the ground

--- checkers/src/battleField.js
+++ checkers/src/battleField.js
@@ -6,7 +6,11 @@ function BattleField() {
         let y = 1;
         let x = 1;
         for(let i = 1; i < 65; i++) {
-            allFields.push(<div className="oneField" data-y={y} data-x={x} key={i}></div>)
+            if(((y === 1 || y === 3) && x % 2 === 1) || (y === 2 && x % 2 === 0)) {
+              allFields.push(<div onClick={addClass} className="oneField whiteChecker" data-y={y} data-x={x} key={i}></div>)
+            } else {
+              allFields.push(<div onClick={addClass} className="oneField" data-y={y} data-x={x} key={i}></div>)
+            }
             x++
             if(x === 9) {
                 x = 1;
@@ -15,11 +19,27 @@ function BattleField() {
         }
         return allFields;
     }
+    const fieldsArray = fieldGenerator();
     
-    console.log(fieldGenerator())
+    // fieldsArray[0].props 
+    // const [state, setState] = useState(fieldsArray[0].props)
+
+    // setState({
+    //   ...state,
+    // })
+    
+    // console.log(fieldsArray[0].props)
+    // console.log(state[0])
+    function addClass (event) {
+      // event.target.className=('sss')
+      if(event.target.className.includes('whiteChecker')) {
+        event.target.className=(event.target.className + ' activeElement')
+      }
+      console.log(event.target)
+    }
   return (
     <div className="battleField">
-      {fieldGenerator()}
+      {fieldsArray}
     </div>
   );
 }
